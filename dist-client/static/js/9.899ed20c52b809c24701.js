97a7b9a89c965e7ce3377c1960c59b871d015dec
zhongxiang1999 <github@zx.com>
2021-12-16 16:10:14 +0800

    第四次上传

---
+++ dist-client/static/js/9.899ed20c52b809c24701.js
@@ -0,0 +1,2 @@
+webpackJsonp([9],{ZIEn:function(e,t){},c5Mg:function(e,t,r){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var n={render:function(){this.$createElement;this._self._c;return this._m(0)},staticRenderFns:[function(){var e=this.$createElement,t=this._self._c||e;return t("div",{staticClass:"error-page"},[t("div",{staticClass:"error-code"},[this._v("404")])])}]};var s=r("VU/8")({name:"error-page"},n,!1,function(e){r("ZIEn")},"data-v-f673bdc6",null);t.default=s.exports}});
+//# sourceMappingURL=9.899ed20c52b809c24701.js.map
--- music-manage/src/api/config.js
+++ music-manage/src/api/config.js
@@ -1,1 +1,2 @@
-export const BASE_URL = 'http://localhost:8888'
+//export const BASE_URL = 'http://localhost:8888'
+export const BASE_URL = 'http://192.168.43.199:8888';//后端服务器的地址（+端口号）
